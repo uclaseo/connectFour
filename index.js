1010f9e3402edc79e724deb554c07d9602a00216
Inseok Seo <inseok.ucla@gmail.com>
2019-07-19 15:04:51 -0700

    Implemented basic rotating players and input system

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ class ConnectFour {
       [null, null, null, null, null, null, null],
       [null, null, null, null, null, null, null],
       [null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null],
+      [null, null, null, 'y', null, null, null],
     ];
+    this.currentPlayer = 'r'
   }
 
   startGame() {
@@ -25,12 +26,28 @@ class ConnectFour {
   
   placeMove() {
     this.printBoard();
-    rl.question('which column?', (answer) => {
-      if (answer === 'exit') {
+    rl.question('Which column? (0 - 6)', (column) => {
+      const isValidInput = Boolean(!isNaN(column) && parseInt(column) <= 6 && parseInt(column) >= 0);
+      if (isValidInput) {
+        const columnIndex = parseInt(column);
+        for (let i = 5; i >= 0; i -= 1){
+          if (!this.board[i][columnIndex]) {
+            this.board[i][columnIndex] = this.currentPlayer;
+            break;
+          }
+        }
+        const hasWinner = this.checkWinner();
+        if (hasWinner) {
+          this.exitGame();
+        } else {
+          this.rotatePlayer();
+          this.placeMove();
+        }
+      } else if (column === 'exit') {
         this.exitGame();
       } else {
+        console.log('Please enter valid input. (0 - 6)');
         this.placeMove();
-        this.checkWinner();
       }
     });
   }
@@ -54,8 +71,16 @@ class ConnectFour {
     console.log(boardString);
   }
 
-  checkWinner() {
+  rotatePlayer() {
+    if (this.currentPlayer === 'y') {
+      this.currentPlayer = 'r';
+    } else {
+      this.currentPlayer = 'y';
+    }
+  }
 
+  checkWinner() {
+    return false;
   }
 
   exitGame() {
